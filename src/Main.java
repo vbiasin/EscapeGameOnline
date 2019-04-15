b2e555623b68f717ca5afabcf18ce77dcd9b333c
Vincent130 <vincent.biasin@outlook.fr>
2019-04-15 19:20:18 +0200

    The generation of a random combination works !

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import EscapeGameOnline.Combination;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Combination test = new Combination();
+        for(int i=0; i<test.getCombinationLength(); i++){
+            System.out.println(test.getCombination()[i]);
+        }
+
 
     }
 }
