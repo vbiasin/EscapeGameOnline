5994d9a3b451ef72994283dd4d1dd0267c21078d
Vincent130 <vincent.biasin@outlook.fr>
2019-04-15 19:02:32 +0200

    Add Test for randomFigure(). Add getters + setters + default constructor in Combination. Add the corpse of the function randomFigure();

--- Tests/CombinationTests.java
+++ Tests/CombinationTests.java
@@ -1,2 +1,18 @@
+import EscapeGameOnline.Combination;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CombinationTests {
+
+    Combination mock = new Combination();
+    @Test
+    public void testerNombre(){
+        for(int i=0; i<100000; i++){
+            int randNumber = mock.randomFigure();
+            assertTrue(((randNumber>=0)&&(randNumber<=9)));
+        }
+
+    }
+
 }
--- src/EscapeGameOnline/Combination.java
+++ src/EscapeGameOnline/Combination.java
@@ -5,5 +5,36 @@ public class Combination {
     private int combinationLength;
     private int combination [];
 
+    public Combination() {
+        this.combinationLength=4;
 
+    }
+
+    /**
+     * Getters and Setters
+     */
+    public int getCombinationLength() {
+        return combinationLength;
+    }
+
+    public void setCombinationLength(int combinationLength) {
+        this.combinationLength = combinationLength;
+    }
+
+    public int[] getCombination() {
+        return combination;
+    }
+
+    public void setCombination(int[] combination) {
+        this.combination = combination;
+    }
+
+    /**
+     * this function return a random number between 0 and 9 both included
+     * int number = (int)(Math.random() * (max+1-min)) + min;
+     * @return int
+     */
+     public int randomFigure(){
+         return -1;
+     }
 }
