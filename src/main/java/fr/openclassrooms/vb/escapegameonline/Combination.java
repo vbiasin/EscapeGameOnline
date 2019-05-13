4f081d4f802a4b3f2669bb10edc06a67763c792e
Vincent130 <vincent.biasin@outlook.fr>
2019-05-13 20:59:26 +0200

    add two functions in Combination class: -checkCurrentPosition which check if value at given position is between 0 and 9 -setCombinationNumberAtPosition which set a value in Combination at given position
    add function setCombnationFromScanner in class Player in which the user can give his own combination.

--- src/main/java/fr/openclassrooms/vb/escapegameonline/Combination.java
+++ src/main/java/fr/openclassrooms/vb/escapegameonline/Combination.java
@@ -30,6 +30,10 @@ public class Combination {
         this.combination = RandomUtil.generateCombination(this.combinationLength);
     }
 
+    public Combination(int combinationLength) {
+        this.combinationLength = combinationLength;
+    }
+
     /**
      * Getters and Setters
      */
@@ -49,6 +53,8 @@ public class Combination {
         this.combination = combination;
     }
 
+
+
     /**
      * This function must compare two combinations
      * Compare each number from Computer Combination with combination given by user.
@@ -58,6 +64,31 @@ public class Combination {
     public void compareCombination(Combination combination){
 
     }
+
+
+    /**
+     * This function must check value at current position
+     * Check if value at position is between 0 and 9 included
+     @parm int position
+     */
+    public boolean checkCurrentNumber(int position)  {
+        boolean check = true;
+        if(this.getCombination()[position]<0 || this.getCombination()[position]>9){
+            check = false;
+        }
+        return check;
+
+    }
+
+    /**
+     * This function put a value at the position check value at current position
+     * Check if position exist !
+     @parm int position ; int value
+     */
+    public void setCombinationNumberAtPosition(int position, int value) throws  ArrayIndexOutOfBoundsException{
+        this.combination[position] = value;
+    }
+
 
 
 
