4fab117d9ad05820f7bc081c6dead8378cac3402
Vincent130 <vincent.biasin@outlook.fr>
2019-05-30 17:56:47 +0200

    add constructor, getters and setters from class Computer

--- src/main/java/fr/openclassrooms/vb/escapegameonline/Computer.java
+++ src/main/java/fr/openclassrooms/vb/escapegameonline/Computer.java
@@ -1,4 +1,44 @@
 package fr.openclassrooms.vb.escapegameonline;
 
+
 public class Computer {
+
+    private int middle;
+    private int min;
+    private int max;
+
+    public Computer(Combination combination) {
+        this.min = 0;
+        this.max = combination.getCombinationLength() ;
+        this.middle = (min+max)/2;
+    }
+
+    /**
+     * Getters and Setters from Attributs of Class
+     */
+    public int getMiddle() {
+        return middle;
+    }
+
+    public void setMiddle(int middle) {
+        this.middle = middle;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+
 }
